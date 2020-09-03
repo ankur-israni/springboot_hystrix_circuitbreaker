35e803ae4552c3c9604ca6a996513e0cf80e3c5a
Ankur Israni <ankur.israni@aa.com>
2020-09-03 02:35:22 -0500

    all done

--- src/main/java/com/ankur/inventory/boot/InventoryApplication.java
+++ src/main/java/com/ankur/inventory/boot/InventoryApplication.java
@@ -3,8 +3,10 @@ package com.ankur.inventory.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication(scanBasePackages = {"com.ankur"})
+@EnableHystrix
 public class InventoryApplication {
 
     public static void main(String[] args) {
